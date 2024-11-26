e120daa59b1f223819a059a2a9507ef095e670d3
choeunsung21 <eesvcho@gmail.com>
2024-11-26 16:05:11 +0900

    [ces] 2차 commit

---
+++ lec02_variable/src/com/gn/practice/Practice01.java
@@ -0,0 +1,15 @@
+package com.gn.practice;
+
+public class Practice01 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 100;
+		int c = a+b;
+		System.out.println(c);
+		System.out.println(a*c);
+		System.out.println((a*c)/a);
+
+	}
+
+}
