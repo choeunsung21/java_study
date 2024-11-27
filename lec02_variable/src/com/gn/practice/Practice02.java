ddf7f18d21f04ddf71e8b61841ff2eb86357e635
choeunsung21 <eesvcho@gmail.com>
2024-11-27 10:21:22 +0900

    3차 commit

---
+++ lec02_variable/src/com/gn/practice/Practice02.java
@@ -0,0 +1,25 @@
+package com.gn.practice;
+
+public class Practice02 {
+
+	public static void main(String[] args) {
+		boolean isStudent = true;
+		System.out.println(isStudent);
+		char grade = 'A';
+		System.out.println(grade);
+		byte candy = 1;
+		System.out.println(candy);
+		short walk = 1000;
+		System.out.println(walk);
+		int day = 3;
+		System.out.println(day);
+		long future = 190;
+		System.out.println(future);
+		float height = 174.2f;
+		System.out.println(height);
+		double score = 4.1;
+		System.out.println(score);
+
+	}
+
+}
