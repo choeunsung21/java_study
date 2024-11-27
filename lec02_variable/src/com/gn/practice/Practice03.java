3c07263720144ccd75bfafad30385998c641c15a
choeunsung21 <eesvcho@gmail.com>
2024-11-27 14:27:36 +0900

    5차 Scanner (nextLine비교) commit

---
+++ lec02_variable/src/com/gn/practice/Practice03.java
@@ -0,0 +1,13 @@
+package com.gn.practice;
+
+public class Practice03 {
+
+	public static void main(String[] args) {
+		
+		String name = new String("제갈민");
+		char firstName = name.charAt(2);
+		System.out.println(firstName+"이에요!!");
+		
+	}
+
+}
